bf574224915f10fe85047805dbd3f5e2f4b5e9ad
tgama85 <tgama85@gmail.com>
2021-05-08 17:05:19 -0300

    Atualizando JS

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,17 @@
+function operacao(){
+  let num = document.getElementById('numtxt')
+  let tab = document.getElementById('tabuadasel')
+  if (num.value.length == 0){
+    window.alert('Por favor, digite um número!')
+  } else{
+    let valor = Number(num.value)
+    let contador = 1
+    while (contador <= 10){
+      let item = document.createElement('option')
+      //criando o option de forma dinâmica
+      item.text =  `${valor} x ${contador} = ${valor*contador}`
+      tab.appendChild(item)
+      contador++
+    }
+  }
+}
