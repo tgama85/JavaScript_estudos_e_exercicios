8332284390924841587ac2fff7ab0630f14aeb7e
tgama85 <tgama85@gmail.com>
2021-05-08 16:33:16 -0300

    Atualização do HTML

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,62 +1,29 @@
 function contar(){
-  var inicio = document.getElementById('inicio')
-  //var fim = document.getElementById('fim')
-  var passo = document.getElementById('passo')
-  //var res = document.querySelector('div#res')
+  let inicio = document.getElementById('iniciotxt')
+  let fim = document.getElementById('fimtxt')
+  let passo = document.getElementById('passotxt')
+  let res = document.querySelector('div#res')
   
-  var n1 = Number(inicio.value)
-  var n2 = Number(passo.value)
-  soma = n1 + n2
-  
-  
-  for (var i = soma; i < 9; i++){
-    
-    i += n2
-    console.log(`${i}`) 
-    
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+      res.innerHTML = 'Impossível contar!'
+    } else{
+      res.innerHTML = 'Contando: '
+      let ini = Number(inicio.value)
+      let fi = Number(fim.value)
+      let pas = Number(passo.value)
+      
+      if (ini < fi){
+        //contador crescente
+        for ( let contador = ini; contador <= fi; contador += pas){
+          res.innerHTML += ` ${contador} \u{1F449}`
+        }
+      }else{
+        //contador decrescente
+        for (let contador = ini; contador >= fi; contador -=pas){
+          res.innerHTML += `${contador} \u{1F449}`
+        }
+      }
+      res.innerHTML += `\u{1F3C1}`
+    }
   }
-
-  /*
-  while (resultado <= fim.value){
-    var n1 = Number(inicio.value)
-    var n2 = Number(passo.value)
-    soma = n1 + n2
-    resultado = soma
-    resultado += n2
-    res.innerHTML = `${resultado}.`
-    break
-  }
-  */
-  
-
-  /*
-  while (numero !== fim){
-    var resultado = 0
-    resultado = numero + passo
-    resultado++
-    
-    res.innerHTML = `Contando: ${resultado.value}`
-    break
-  } 
-  */
-  
-  
-  //res.innerHTML = `O número é ${inicio}, o fim é ${fim} e o passo é ${passo}`
-}
-
-
-/*
-function contar(){
- 
-  for (var i = 0; i <= fim; i++){
-    var numero = document.querySelector('div#inicio')
-    var fim = document.querySelector('div#fim')
-    var passo = document.querySelector('div#passo')
-    var res = document.querySelector('div#res')
-
-    i = numero
-
-    res.innerHTML = `${i}`
-  }
-}
-*/
+  //unicode emoji list
