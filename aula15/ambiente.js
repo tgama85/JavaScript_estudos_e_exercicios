dbad2e6aa3bb09f8981ff0f2c867a93799e0c8f4
tgama85 <tgama85@gmail.com>
2021-05-08 18:57:03 -0300

    Testes com Array

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5,9,1,8]
+
+console.log(`Nosso valor é ${num}`)//mostra os valores
+console.log(num) //mostra os valores dentro do array
+console.log(`O valor tem ${num.length} posições`) //mostra o número de elementos do array
+console.log(`${num[1]}`) //mostra o valor da posição 1
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+num.sort() //método interno que organiza o array em ordem crescente
+console.log(num) //array ordenado
+num.push(3) //adiciona um número na última posição
+console.log(num) //mostrando o número adicionado
