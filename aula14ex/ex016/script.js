ee7b6cffdcc8e23a5ebd0dcb091510c868e97cb3
tgama85 <tgama85@gmail.com>
2021-05-08 16:39:53 -0300

    Atualização JS

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -7,11 +7,16 @@ function contar(){
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
       res.innerHTML = 'Impossível contar!'
     } else{
-      res.innerHTML = 'Contando: '
+      res.innerHTML = 'Contando: <br>'
       let ini = Number(inicio.value)
       let fi = Number(fim.value)
       let pas = Number(passo.value)
       
+      if (pas <= 0 ){
+        window.alert('Passo inválido! Considerando PASSO 1')
+        pas = 1
+      }
+
       if (ini < fi){
         //contador crescente
         for ( let contador = ini; contador <= fi; contador += pas){
